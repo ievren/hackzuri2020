8e679a2f3194b33035501c3e85f6f2c298fde6c8
Davud Evren <devren@student.ethz.ch>
2020-03-28 16:38:41 +0100

    init aws amplify

---
+++ myAmplifyReportingTool/src/graphql/subscriptions.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      name
+      description
+      detChef
+      abfahrt
+      ankunft
+      kilometer
+      liter
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      name
+      description
+      detChef
+      abfahrt
+      ankunft
+      kilometer
+      liter
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      name
+      description
+      detChef
+      abfahrt
+      ankunft
+      kilometer
+      liter
+    }
+  }
+`;
---
+++ src/graphql/queries.js
@@ -0,0 +1,38 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getTodo = /* GraphQL */ `
+  query GetTodo($id: ID!) {
+    getTodo(id: $id) {
+      id
+      name
+      description
+      detChef
+      abfahrt
+      ankunft
+      kilometer
+      liter
+    }
+  }
+`;
+export const listTodos = /* GraphQL */ `
+  query ListTodos(
+    $filter: ModelTodoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        description
+        detChef
+        abfahrt
+        ankunft
+        kilometer
+        liter
+      }
+      nextToken
+    }
+  }
+`;
